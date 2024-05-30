6c84d8e29cc6436581119981feba8ef9026fc0a3
Panagiotis Deligiannis <deligianp@athenarc.gr>
2024-05-30 11:55:17 +0300

    Update ApiKeyLogin and create components for different prompts

--- schema-labs/src/auth/ApiKeyLogin.jsx
+++ schema-labs/src/auth/ApiKeyLogin.jsx
@@ -1,53 +1,31 @@
-import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
+import React, { useContext, useState } from "react";
 import Button from 'react-bootstrap/Button';
-import { UserDetailsContext } from "../utils/AuthProvider";
-import { useContext } from "react";
+import Form from "react-bootstrap/Form";
+import { UserDetailsContext } from "../utils/components/auth/AuthProvider";
 
 const ApiKeyLogin = props => {
     const [apiKey, setApiKey] = useState(null);
-    const {userDetails, setUserDetails} = useContext(UserDetailsContext);
-
-    const handleLogout = event => {
-        event.preventDefault();
-        setUserDetails(null);
-    }
+    const { userDetails, setUserDetails } = useContext(UserDetailsContext);
 
     const handleLogin = event => {
         event.preventDefault();
-        console.log('Got API key: ', apiKey);
-        setApiKey(null);
         setUserDetails({
             apiKey,
             type: "apiKey"
         });
     };
     if (userDetails) {
-        return <Container>
-            <span>Logged in with {userDetails.apiKey}</span>
-            <span>Current inputState: {String(apiKey)}</span>
-            <Form onSubmit={handleLogout}>
-                <Button variant="primary" type="submit">
-                    Log out
-                </Button>
-            </Form>
-        </Container>
+        return <span>Already logged in</span>
     }
-    return <Container>
-        <Form onSubmit={handleLogin}>
-            <span>Current inputState: {String(apiKey)}</span>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>API key</Form.Label>
-                <Form.Control type="password" placeholder="API key" onChange={e=> setApiKey(e.target.value)}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Log in
-            </Button>
-        </Form>
-    </Container>
+    return <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="apiKeyInput">
+            <Form.Label>API key</Form.Label>
+            <Form.Control type="password" placeholder="API key" onChange={e => setApiKey(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+            Log in
+        </Button>
+    </Form>
 }
 
 export default ApiKeyLogin;
--- schema-labs/src/auth/index.jsx
+++ schema-labs/src/auth/index.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import { useNavigate } from "react-router-dom";
+import { UserDetailsContext } from "../utils/components/auth/AuthProvider";
+import LoginPrompt from "./LoginPrompt";
+import LogoutPrompt from "./LogoutPrompt";
 
-const Login = props => {
-    return <div>Login</div>
+const Auth = () => {
+    const { userDetails } = useContext(UserDetailsContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (userDetails) {
+            navigate('/dashboard');
+        }
+    }, [userDetails]);
+    const internal = userDetails
+        ? <LogoutPrompt />
+        : <Row>
+            <Col className="border-end border-muted-subtle">{/* Placeholder for potential register prompt */}</Col>
+            <Col className="p-4">
+                <LoginPrompt />
+            </Col>
+        </Row>
+
+    return <Row className="justify-content-center p-1">
+        <Col className="border border-light-subtle rounded p-4">
+            {internal}
+        </Col>
+    </Row>
 }
 
-export default Login;
+export default Auth;
