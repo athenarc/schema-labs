28961085842e1b2a4e6debd6dd951e202f44c0fb
Nikos Foutris <nikosfo@gmail.com>
2024-06-20 00:37:55 +0300

    Modify search results message #183

--- schema-lab/src/dashboard/tasks/TasksPaginationControls.jsx
+++ schema-lab/src/dashboard/tasks/TasksPaginationControls.jsx
@@ -29,7 +29,7 @@ const TasksPaginationControls = () => {
             <Col className="d-flex justify-content-start align-items-start">
                 {count > pageSize
                     ? <small>Displaying tasks {(currentPageIndex * pageSize) + 1}-{Math.min((currentPageIndex + 1) * pageSize, count)}, out of {count} total</small>
-                    : <small>Displaying {count} total tasks</small>
+                    : <small>Search results: {count} total tasks</small>
                 }
             </Col>
             <Col className="d-flex justify-content-end align-items-start">
