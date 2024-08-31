609d4e40baf8e7e38ccf2c5308f66c7fb123c709
Nikos Foutris <nikosfo@gmail.com>
2024-08-31 16:18:49 +0300

    Set color of schema lab text to purple

--- schema-lab/src/layouts/Footer.jsx
+++ schema-lab/src/layouts/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-light text-center">
             <p>
-                Powered by <a href="https://github.com/athenarc/schema-lab">SCHEMA lab</a>, 
+                Powered by <a href="https://github.com/athenarc/schema-lab" style={{color: '#7700D4'}}>SCHEMA lab</a>, 
                 <br /> Copyright © 2024, HYPATIA lab, ATHENA RC.
             </p>
         </footer>
