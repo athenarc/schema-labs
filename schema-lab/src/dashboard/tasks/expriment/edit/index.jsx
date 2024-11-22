34e971e2f29ad5393e3a861522f325d847ff0568
nikosfo <nikosfo@gmail.com>
2024-11-22 23:29:50 +0200

    Fixup experiments sorting and drop submission time column (#54)

--- schema-lab/src/dashboard/tasks/expriment/edit/index.jsx
+++ schema-lab/src/dashboard/tasks/expriment/edit/index.jsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PatchExperiment from "./ExperimentEdit";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { Container } from "react-bootstrap";
- 
-const EditExperiment = () => {
 
+const EditExperiment = () => {
     const [nextClicked, setNextClicked] = useState(true);
 
-
     return (
-        <>
         <div className="d-flex flex-column min-vh-100">
-        <Container className="flex-grow-1">
-            <Row>
-                <Col>
-                    <Container>
+            <Container className="flex-grow-1">
+                <Row>
+                    <Col>
+                        <Container>
                             <PatchExperiment />
-                    </Container>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-    </>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
